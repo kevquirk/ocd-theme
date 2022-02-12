b6bc0e017a9a7d0be26efce15f45187531aad3b9
Kev Quirk <kev@quirk.cc>
2022-02-12 12:39:49 +0000

    Added mailto function for email button

--- functions.php
+++ functions.php
@@ -6,7 +6,14 @@
  * Only edit this file if you have direct access to it on your server (to fix errors if they happen).
  */
 
- // Gutenberg custom stylesheet
- add_theme_support('editor-styles');
- add_editor_style( 'style.css' );
- add_editor_style( 'editor-style.css' );
+// Gutenberg custom stylesheet
+add_theme_support('editor-styles');
+add_editor_style( 'style.css' );
+add_editor_style( 'editor-style.css' );
+
+// Add shortcode for reply via mail link
+add_shortcode( 'mailto_title', 'mailto_title' );
+
+function mailto_title( $atts ) {
+    return esc_attr( get_the_title( get_the_ID() ) );
+}
