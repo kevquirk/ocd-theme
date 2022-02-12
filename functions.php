c9a75d1bc108d25f9824f490597780ae2a14f585
Kev Quirk <kev@quirk.cc>
2022-02-12 12:18:43 +0000

    editor css

--- functions.php
+++ functions.php
@@ -5,3 +5,8 @@
  * Add your custom PHP in this file.
  * Only edit this file if you have direct access to it on your server (to fix errors if they happen).
  */
+
+ // Gutenberg custom stylesheet
+ add_theme_support('editor-styles');
+ add_editor_style( 'style.css' );
+ add_editor_style( 'editor-style.css' );
