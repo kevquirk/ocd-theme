4158d2a38ca57ee5c42eb15fa0e13f95a86d2e0e
Kev Quirk <kev@quirk.cc>
2022-03-14 16:22:27 +0000

    Added RSS email reply.

--- functions.php
+++ functions.php
@@ -17,3 +17,12 @@ add_shortcode( 'mailto_title', 'mailto_title' );
 function mailto_title( $atts ) {
     return esc_attr( get_the_title( get_the_ID() ) );
 }
+
+// Add reply link to RSS feed
+add_filter( "the_content_feed", "feed_comment_via_email" );
+
+function feed_comment_via_email($content)
+{
+   $content .= "<p><a href=\"mailto: " . get_the_author_meta('user_email') . "?subject=RE: '" . get_the_title() . "'&body=Post link: " . get_permalink() . "\">Reply via email</a></p>";
+   return $content;
+}
